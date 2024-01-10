217f85644653227751d05d1bea9587af53be3b40
MHZ0 <mohamedhadizaira@gmail.com>
2024-01-10 22:36:39 +0100

    added UI for Clients to order

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div className="mb-4">
             <div className="flex justify-center rounded-b bg-red-700 text-2xl 
-            border-b border-gray-500 shadow-lg font-bold py-2 px-4
+            border-b border-gray-700 shadow-lg font-bold py-2 px-4
             subpixel-antialiased ">
                 <h1>Welcome To Your Favorite Fast Food Restaurant</h1>
             </div>
